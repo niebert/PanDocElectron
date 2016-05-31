8c1a60c81b2d032631489e6b93309263883c4a68
Bert Niehaus <niehausbert@gmail.com>
2016-05-31 16:26:47 +0200

    Add Install Scripts for Linux and Max. Windows is missing.

---
+++ install/install_mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Install Electron Framework"
+echo "--------------------------"
+DOCUMENTS="Documents"
+sudo apt-get install git npm curl
+sudo apt-get install -y build-essential
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo "Install PanDocElectron Requirements"
+echo "-----------------------------------"
+npm install mkdirp
+sudo apt-get install pandoc pandoc-citeproc imagemagick imagemagick-doc texlive-full
+cd ~
+git clone https://github.com/niebert/PanDocElectron.git
+cd PanDocElectron
+rm -R node_modules/*
+npm install electron-prebuilt --save-dev
+cd ..
+cd $DOCUMENTS
+git clone https://github.com/niebert/PanDoc.git
