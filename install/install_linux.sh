bd69bdb2770c2ffd389f517cbba88f8231746f18
Bert Niehaus <niehausbert@gmail.com>
2016-07-06 22:11:37 +0200

    Install Scripts updated cd DOCUMENTS

--- install/install_linux.sh
+++ install/install_linux.sh
@@ -3,12 +3,15 @@ echo "Install Electron Framework"
 echo "--------------------------"
 echo "OS: Debian, Ubuntu, Mint"
 DOCUMENTS="Documents"
+cd ~
+cd $DOCUMENTS
 sudo apt-get install git npm curl
 sudo apt-get install -y build-essential
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 echo "Install PanDocElectron Requirements"
 echo "-----------------------------------"
+echo "OS: Debian, Ubuntu, Mint"
 npm install mkdirp
 sudo apt-get install pandoc pandoc-citeproc imagemagick imagemagick-doc texlive-full
 cd ~
--- install/install_mac.sh
+++ install/install_mac.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 echo "Install Electron Framework"
 echo "--------------------------"
+echo "OS: MacOSX-Installer"
 DOCUMENTS="Documents"
-sudo apt-get install git npm curl
-sudo apt-get install -y build-essential
+cd ~
+cd $DOCUMENTS
+brew install git npm curl
+brew install Caskroom/cask/xquartz wine mono
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 echo "Install PanDocElectron Requirements"
 echo "-----------------------------------"
+echo "OS: MacOSX-Installer"
 npm install mkdirp
 sudo apt-get install pandoc pandoc-citeproc imagemagick imagemagick-doc texlive-full
 cd ~
