16e41220d7999ed11431f1e80e93e5d247f0eea1
Bert Niehaus <niehausbert@gmail.com>
2016-07-14 10:29:57 +0200

    package script improved - rename

--- packageapp.sh
+++ packageapp.sh
@@ -14,6 +14,8 @@ rm -R "${AppName}-mas-x64"
 
 vArchOld="x64"
 vArchNew="64Bit"
+echo "Create for Architecture ${vArchNew} (${vArchOld})"
+
 vOSOld="darwin"
 vOSNew="MacOSX"
 cmv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
@@ -21,7 +23,7 @@ zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-$
 # sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 
 vOSOld="win32"
-vOSNew="Win32"
+vOSNew="Windows"
 mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
@@ -34,12 +36,14 @@ sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 
 vArchOld="ia32"
 vArchNew="32Bit"
+echo "Create for Architecture ${vArchNew} (${vArchOld})"
+
 mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 
 vOSOld="win32"
-vOSNew="Win32"
+vOSNew="Windows"
 mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
 sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
