e5318b40c08559ce81d35ef5b664c3d0ed6ba7d3
Bert Niehaus <niehausbert@gmail.com>
2016-08-10 17:41:51 +0200

    INSTALL SCRIPTS OPTIMISATION: Make a Backup if PanDoc and PanDocElectron exists

--- install/install_linux.sh
+++ install/install_linux.sh
@@ -33,11 +33,15 @@ WORKING_DIR="PanDocElectron"
 if [ -d "$WORKING_DIR" ]; then mv $WORKING_DIR "${WORKING_DIR}_bak"; fi
 git clone https://github.com/niebert/PanDocElectron.git
 cd PanDocElectron
-# rm -R node_modules/*
-WORKING_DIR="node_modules"
+#WORKING_DIR="node_modules"
+echo "Delete '/${WORKING_DIR}' if exists"
 if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
-sudo apt-get install npm
-npm install electron-prebuilt --save-dev
+npm install
 cd ..
-cd $DOCUMENTS
+WORKING_DIR="PanDoc_bak_bak"
+if [ -d "$WORKING_DIR" ]; then rm -r ${WORKING_DIR}; fi
+WORKING_DIR="PanDoc_bak"
+if [ -d "$WORKING_DIR" ]; then mv ${WORKING_DIR} "${WORKING_DIR}_bak"; fi
+WORKING_DIR="PanDoc"
+if [ -d "$WORKING_DIR" ]; then mv $WORKING_DIR "${WORKING_DIR}_bak"; fi
 git clone https://github.com/niebert/PanDoc.git
--- install/install_mac.sh
+++ install/install_mac.sh
@@ -12,13 +12,32 @@ sudo apt-get install -y nodejs
 echo "Install PanDocElectron Requirements"
 echo "-----------------------------------"
 echo "OS: MacOSX-Installer"
-npm install mkdirp
 sudo apt-get install pandoc pandoc-citeproc imagemagick imagemagick-doc texlive-full
-cd ~
+WORKING_DIR="PanDocElectron_bak_bak"
+if [ -d "$WORKING_DIR" ]; then rm -r ${WORKING_DIR}; fi
+WORKING_DIR="PanDocElectron_bak"
+if [ -d "$WORKING_DIR" ]; then mv ${WORKING_DIR} "${WORKING_DIR}_bak"; fi
+WORKING_DIR="PanDocElectron"
+if [ -d "$WORKING_DIR" ]; then mv $WORKING_DIR "${WORKING_DIR}_bak"; fi
 git clone https://github.com/niebert/PanDocElectron.git
 cd PanDocElectron
-rm -R node_modules/*
-npm install electron-prebuilt --save-dev
+WORKING_DIR="node_modules"
+echo "Delete '/${WORKING_DIR}' if exists"
+if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
+npm install
+echo "Install PanDoc Templates"
+cd ..
+WORKING_DIR="PanDoc_bak_bak"
+if [ -d "$WORKING_DIR" ]; then rm -r ${WORKING_DIR}; fi
+WORKING_DIR="PanDoc_bak"
+if [ -d "$WORKING_DIR" ]; then mv ${WORKING_DIR} "${WORKING_DIR}_bak"; fi
+WORKING_DIR="PanDoc"
+if [ -d "$WORKING_DIR" ]; then mv $WORKING_DIR "${WORKING_DIR}_bak"; fi
+git clone https://github.com/niebert/PanDoc.git
+
+# rm -R node_modules/*
+
+npm install
 cd ..
 cd $DOCUMENTS
 git clone https://github.com/niebert/PanDoc.git
