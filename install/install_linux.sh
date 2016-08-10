75870799d060421e338f279e4a869fa6cbf3a3ae
Bert Niehaus <niehausbert@gmail.com>
2016-08-10 14:41:21 +0200

    Updated Install Script and comment on nodejs-legacy added

--- install/install_linux.sh
+++ install/install_linux.sh
@@ -6,16 +6,31 @@ DOCUMENTS="Documents"
 cd ~
 cd $DOCUMENTS
 cd Dokumente
+echo "REMARK: If Installation ends with error 'node not found'"
+echo "please install 'nodejs-legacy' with the following command"
+echo "apt-get install nodejs-legacy"
+echo "--------------------------"
+echo "(1) Install GIT NPM and CURL"
 sudo apt-get install git npm curl
 sudo apt-get install -y build-essential
+echo "--------------------------"
+echo "(2) Install NODEJS"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get update
 sudo apt-get install -y nodejs
-echo "Install PanDocElectron Requirements"
-echo "-----------------------------------"
+echo "--------------------------"
+echo "(3) Install PanDocElectron Requirements"
+echo "---------------------------------------"
 echo "OS: Debian, Ubuntu, Mint"
 npm install mkdirp
 sudo apt-get install pandoc pandoc-citeproc imagemagick imagemagick-doc texlive-full
 # cd ~
+WORKING_DIR="PanDocElectron_bak_bak"
+if [ -d "$WORKING_DIR" ]; then rm -r ${WORKING_DIR}; fi
+WORKING_DIR="PanDocElectron_bak"
+if [ -d "$WORKING_DIR" ]; then mv ${WORKING_DIR} "${WORKING_DIR}_bak"; fi
+WORKING_DIR="PanDocElectron"
+if [ -d "$WORKING_DIR" ]; then mv $WORKING_DIR "${WORKING_DIR}_bak"; fi
 git clone https://github.com/niebert/PanDocElectron.git
 cd PanDocElectron
 # rm -R node_modules/*
