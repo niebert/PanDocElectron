b987df1fdf10ca909b95a2fe05b478c67307a0db
Bert Niehaus <niehausbert@gmail.com>
2016-10-06 16:51:49 +0200

    Update install script Linux and MacOSX furthermore add-menuitem.sh was added as alpha-version to append later to install_linux.sh

--- install_lin/install_linux.sh
+++ install_lin/install_linux.sh
@@ -6,16 +6,18 @@ DOCUMENTS="Documents"
 cd ~
 cd $DOCUMENTS
 cd Dokumente
+echo "REMARK: If Installation ends with error 'node not found'"
+echo "please install 'nodejs-legacy' with the following command"
+echo "apt-get install nodejs-legacy"
 echo "--------------------------"
 echo "(1) Install GIT NPM and CURL"
-sudo apt-get install -y git curl
+sudo apt-get install git npm curl
 sudo apt-get install -y build-essential
 echo "--------------------------"
 echo "(2) Install NODEJS"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs
-sudo apt-get install -y npm
 echo "--------------------------"
 echo "(3) Install PanDocElectron Requirements"
 echo "---------------------------------------"
@@ -43,6 +45,3 @@ if [ -d "$WORKING_DIR" ]; then mv ${WORKING_DIR} "${WORKING_DIR}_bak"; fi
 WORKING_DIR="PanDoc"
 if [ -d "$WORKING_DIR" ]; then mv $WORKING_DIR "${WORKING_DIR}_bak"; fi
 git clone https://github.com/niebert/PanDoc.git
-echo "REMARK: If Installation ends with error 'node not found'"
-echo "please install 'nodejs-legacy' with the following command"
-echo "  sudo apt-get install nodejs-legacy"
